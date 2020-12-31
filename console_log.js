fecb644c9c3d63b7d9d4fc5119763650cc115b63
wahyupambudi <wahyupambudi823@gmail.com>
2020-12-31 13:14:54 +0700

    awonapa-javascript

---
+++ console_log.js
@@ -0,0 +1,15 @@
+console.log("Menampilkan Teks menggunakan Console.log");
+
+var belajarConsole = "Hello World!"
+console.log(belajarConsole);
+
+let namaDepan = "Wahyu"
+console.log(namaDepan);
+
+const namaBelakang = "Pambudi"
+console.log(namaBelakang);
+
+// menggabungkan teks dan variabel pada console.log
+
+console.log("Nama Depan : " + namaDepan + " dan Nama Belakang " + namaBelakang);
+console.log(namaDepan +" "+ namaBelakang);
