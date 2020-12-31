097eef7d57de079f1137dd5ba15c76fb93e636c4
wahyupambudi <wahyupambudi823@gmail.com>
2020-12-31 23:35:05 +0700

    update awonapa blog

---
+++ 2_variable_javascript.js
@@ -0,0 +1,25 @@
+// create wahyupambudi | awonapa
+
+// create variabel with Var
+var infoVar = "Create Variable with var";
+console.log(infoVar);
+
+// create variabel with let
+let infoLet = "Create Variable with let";
+console.log(infoLet);
+
+// create variabel with const
+const infoConst = "Create Variable with const";
+console.log(infoConst);
+
+
+// change value in variable
+
+// change value in var
+infoVar = "Change Variable in var";
+
+// change value in let
+infoLet = "Change Variable in let";
+
+// change value in var
+infoConst = "Change Variable in const";
