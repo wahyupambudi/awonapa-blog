cdbf05d62fbc0bd35b10835c1e615e5f0a709668
wahyupambudi <wahyupambudi823@gmail.com>
2021-01-03 21:12:54 +0700

    add type data null / undefined

--- 3_tipe_data.js
+++ 3_tipe_data.js
@@ -3,4 +3,19 @@
 // ===============================
 
 // tipe data boolean
+// dengan nilai true / false
 var nilai = 100;
+var status;
+
+if (nilai > 99)
+status = true;
+else 
+status = false;
+
+console.log(status); // true
+
+// tipe data null / undefined
+var isi = null;
+var kosong;
+console.log(isi); // null
+console.log(kosong); // undefined 
